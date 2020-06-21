0abe83c79ea0d7b418da447c4ce9b4fcff7ea5b7
Naman Jain <jainnaman335@gmail.com>
2020-06-21 17:09:42 +0530

    connecting to mongo

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
+const db =require('./models/config').MongoURI;
+//db config
+
+//connect to db
+mongoose.connect(db,{useNewUrlParser:true})
+.then(()=> console.log('connected to db'))
+.catch(err => console.log(err));
 
 
 app.get("/", function(req,res){
@@ -24,7 +31,7 @@ app.get("/login", function(req,res){
 })
 
 
-const PORT = 8081 || process.env.PORT
+const PORT = 3000 || process.env.PORT
 
 app.listen(PORT, process.env.IP, function(){
 	console.log("Server has begun!!");
---
+++ models/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://BlogOwner:123456789Owner@friendsblog-esfpd.mongodb.net/test?retryWrites=true&w=majority'
+}
