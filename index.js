f6fe2443178da08be4fe523d326094031faa52b2
Naman Jain <jainnaman335@gmail.com>
2020-06-22 14:49:16 +0530

    post

--- index.js
+++ index.js
@@ -8,32 +8,34 @@ var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+	res.render("landing");
+});
+
+app.get("/signup", function (req, res) {
+	res.render("signup");
+});
+
+app.post("/signup", function (req, res) {
+	console.log(req.body);
+	res.render('hello');
+});
+
 /////////////////////////////////////////////////////////// DB
 
-const db = require('./models/config').MongoURI;
+const db = 'mongodb://localhost:27017/Users'
 //db config
 
 //connect to db
-mongoose.connect( db, 
-	{useNewUrlParser:true,
-	useUnifiedTopology: true,
-	useCreateIndex: true
-})
+mongoose.connect( db, {useNewUrlParser:true,useUnifiedTopology: true,useCreateIndex: true})
 .then(()=> console.log('connected to db'))
 .catch(err => console.log(err));
 ///////////////////////////////////////////////////////////////SERVERS
 
-app.get("/", function(req,res){
-	res.render("landing");
-});
 
-app.get("/signup", function(req,res){
-	res.render("signup");
-})
 
-app.get("/login", function(req,res){
-	res.render("login");
-})
+
 
 
 const PORT = 3000 || process.env.PORT
--- models/config.js
+++ models/config.js
@@ -1,4 +1,1 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://BlogOwner:123456789Owner@friendsblog-esfpd.mongodb.net/Users?retryWrites=true&w=majority'
-}
 
