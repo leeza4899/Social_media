6e66862a91fcfcf822b5cc8e8add5d2e79b41190
leeza4899 <gargleeza4899@gmail.com>
2020-06-23 18:36:14 +0530

    login done

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ const passport      = require("passport");
 const bcrypt 		= require("bcryptjs");
 const LocalStrategy = require("passport-local");
 const flash		    = require("connect-flash");
-const User 			= require("./models/user");
-const { Router } = require("express");
 
+
+//////ROUTE FILES
+var indexRoutes       = require("./routes/authRoutes");
+
+/////MODELS
+const User 			= require("./models/user");
+
+//////moongoose
+const db = 'mongodb://localhost:27017/Users'
+//db config
+
+//connect to db
+mongoose.connect( db, {useNewUrlParser:true,useUnifiedTopology: true,useCreateIndex: true})
+.then(()=> console.log('connected to db'))
+.catch(err => console.log(err));
+
+
+////additional-------
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
@@ -67,113 +83,17 @@ passport.serializeUser(function (user, done) {
 	done(null, user.id);
 });
 passport.deserializeUser(function (id, done) {
-	user.findById(id, function (err, user) {
+	User.findById(id, function (err, user) {
 		done(err, user);
 	});
 });
-//////////////////////////////
 
-//acquiring the db models
-
-
-
-//middleware for login
+////using routes
+app.use(indexRoutes);
 
 
-///////#######AUTH ROUTES#######////////
-//landing route
-app.get("/", function (req, res) {
-	res.render("landing");
-});
 
-//signup routes
-app.get("/signup", function (req, res) {
-	res.render("signup");
-});
-
-app.post("/signup", function (req, res) {
-	//check required
-	const {signname, username, emailid, pass, dob} =req.body;
-	let errors =[];
-		if( !signname || !username || !emailid || !pass || !dob){
-			errors.push({msg : "Fill all fields"})
-		}
-		if(errors.length > 0){
-			console.log(errors)
-			res.render('signup',{
-				errors,
-				signname,
-				username,
-				emailid,
-				dob			
-			});
-		} else { 	
-			//registering new user\
-			User.findOne({email : emailid})
-			.then(user =>{
-				if(user){
-					errors.push({msg:"Email Already Exist"})
-					res.render('signup',{
-				errors,
-				signname,
-				username,
-				emailid,
-				dob			
-			});
-				} else{
-					const newUser = new User({ 
-					name: req.body.signname,
-					email: req.body.emailid,
-					password:req.body.pass,
-					username: req.body.username,
-					date: req.body.dob
-					});
-
-					bcrypt.genSalt(10, (err, salt) => {
-						bcrypt.hash(newUser.password, salt, (err, hash) => {
-						  if (err) throw err;
-						  newUser.password = hash;
-						  newUser.save()
-							.then(user => {
-								req.flash('success_msg',"you are now a member please login");
-							  res.redirect('/signup');
-							})
-							.catch(err => console.log(err));
-						});	
-					});	
-				}
-	});
-}
-});
-
-//login route
-app.post('/login', (req, res, next) => {
-	const{email,password} =req.body;
-	passport.authenticate("local",{
-		successRedirect:'/signup',
-		failureRedirect:'/landing',
-		failureFlash:true
-	})(req,res,next);
-	
-});
-
-//logout route
-app.get("/logout", function(req,res){
-	req.logout();
-	res.send("logout done");
-});
-/////////////////////////////////////////////////////////// DB
-
-const db = 'mongodb://localhost:27017/Users'
-//db config
-
-//connect to db
-mongoose.connect( db, {useNewUrlParser:true,useUnifiedTopology: true,useCreateIndex: true})
-.then(()=> console.log('connected to db'))
-.catch(err => console.log(err));
-///////////////////////////////////////////////////////////////SERVERS
-
-
+/////listening ports
 const PORT = 3000 || process.env.PORT
 
 app.listen(PORT, process.env.IP, function(){
---
+++ routes/authRoutes.js
@@ -0,0 +1,91 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var bcrypt = require("bcryptjs");
+var User = require("../models/user");
+
+
+//landing route
+router.get("/", function (req, res) {
+	res.render("landing");
+});
+
+//signup routes
+router.get("/signup", function (req, res) {
+	res.render("signup");
+});
+
+router.post("/signup", function (req, res) {
+	//check required
+	const {signname, username, emailid, pass, dob} =req.body;
+	let errors =[];
+		if( !signname || !username || !emailid || !pass || !dob){
+			errors.push({msg : "Fill all fields"})
+		}
+		if(errors.length > 0){
+			console.log(errors)
+			res.render('signup',{
+				errors,
+				signname,
+				username,
+				emailid,
+				dob			
+			});
+		} else { 	
+			//registering new user\
+			User.findOne({email : emailid})
+			.then(user =>{
+				if(user){
+					errors.push({msg:"Email Already Exist"})
+					res.render('signup',{
+				errors,
+				signname,
+				username,
+				emailid,
+				dob			
+			});
+				} else{
+					const newUser = new User({ 
+					name: req.body.signname,
+					email: req.body.emailid,
+					password:req.body.pass,
+					username: req.body.username,
+					date: req.body.dob
+					});
+
+					bcrypt.genSalt(10, (err, salt) => {
+						bcrypt.hash(newUser.password, salt, (err, hash) => {
+						  if (err) throw err;
+						  newUser.password = hash;
+						  newUser.save()
+							.then(user => {
+								req.flash('success_msg',"you are now a member please login");
+							  res.redirect('/signup');
+							})
+							.catch(err => console.log(err));
+						});	
+					});	
+				}
+	});
+}
+});
+
+//login route
+router.post('/login', (req, res, next) => {
+	const{email,password} =req.body;
+	passport.authenticate("local",{
+		successRedirect:'/',
+		failureRedirect:'/signup',
+		failureFlash:true
+	})(req,res,next);
+	
+});
+
+//logout route
+router.get("/logout", function(req,res){
+	req.logout();
+	res.send("logout done");
+});
+
+
+module.exports = router;
